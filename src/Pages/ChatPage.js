4b543133dde7874c340086510157e428536d59da
Dor Levi <dorlev1204@gmail.com>
2022-05-22 20:14:48 +0300

    signalR working

--- src/Pages/ChatPage.js
+++ src/Pages/ChatPage.js
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ChatSideBar from '../Components/ChatSideBar';
 import ChatContent from '../Components/ChatContent';
 import './ChatPage.css';
 import useWindowDimensions from '../Logic/window';
+import { HubConnectionBuilder, HttpTransportType } from '@microsoft/signalr';
+import Token from '../Token.js';
 
 function ChatPage(props){
 
@@ -10,6 +12,8 @@ function ChatPage(props){
     const [contactServer, setContactServer] = useState("");
     const [refreshed, setRefreshed] = useState(false);
     const { height, width } = useWindowDimensions();
+
+    const [ connection, setConnection ] = useState(null);
     function isEmpty() {
         if (contact=="Empty") {
             return (<div></div>);
@@ -17,6 +21,34 @@ function ChatPage(props){
             return (<ChatContent refreshed={refreshed} setRefreshed={setRefreshed} username={props.username} contact={contact} contactServer={contactServer}></ChatContent>);
         }
     }
+    ;
+
+    useEffect(() => {
+        const newConnection = new HubConnectionBuilder()
+            .withUrl('https://localhost:7267/ChatHub', {
+                accessTokenFactory: () => Token.get(),
+                skipNegotiation: true,
+                transport: HttpTransportType.WebSockets
+            })
+            .withAutomaticReconnect()
+            .build();
+
+        setConnection(newConnection);
+    }, []);
+
+    useEffect(() => {
+        if (connection) {
+            connection.start()
+                .then(result => {
+                    console.log('Connected!');
+    
+                    connection.on('ReceiveMessage', message => {
+                        setRefreshed((prev) => {return !prev})
+                    });
+                })
+                .catch(e => console.log('Connection failed: ', e));
+        }
+    }, [connection]);
 
     return(
             <div className="container bbb">
